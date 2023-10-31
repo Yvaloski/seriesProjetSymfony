68ed0f5c1f20c335347ca0b79ece86a4f5bb07c6
yvaloski <yalorvellec@gmail.com>
2023-10-31 16:59:26 +0100

    Mise en place page des pages indx show

---
+++ src/Controller/SerieController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/serie")
+ */
+class SerieController extends AbstractController
+{
+    /**
+     * @Route("/", name="serie_index")
+     */
+    public function index(): Response
+    {
+        //TODO: Récuperation de la liste des séries en bdd
+
+        return $this->render('serie/index.html.twig');
+    }
+
+    /**
+     * @Route("/new", name="serie_new")
+     */
+    public function new():Response
+    {
+        return $this->render('serie/new.html.twig');
+    }
+
+    /**
+     * @Route("/{id}", name="serie_show", requirements={"id"="\d+"})
+     */
+    public function show(int $id):Response
+    {
+        //TODO: Récupérer la serie correspondante a $id
+        return $this->render('serie/show.html.twig',[
+            'id'=>$id
+
+        ]);
+    }
+
+
+
+
+}
